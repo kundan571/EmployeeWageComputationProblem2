5b7a55aafd69f2afa452d4c7702f3ab4a2be8aa1
kundan571 <kundanchauhan1997@gmail.com>
2023-02-02 15:41:50 +0530

    Added: Welcome message

---
+++ src/employeeWage/EmpWageBuilder.java
@@ -0,0 +1,8 @@
+package employeeWage;
+
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Problem");
+
+    }
+}
