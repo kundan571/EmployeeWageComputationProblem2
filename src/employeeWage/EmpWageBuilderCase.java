f5d575c578b92276e2910fb5834f809f9f947d88
kundan571 <kundanchauhan1997@gmail.com>
2023-02-03 14:37:57 +0530

    Calculated employee wage using switch

---
+++ src/employeeWage/EmpWageBuilderCase.java
@@ -0,0 +1,28 @@
+package employeeWage;
+
+public class EmpWageBuilderCase {
+    // Constant
+    public static final int IS_PART_TIME = 1;
+    public static final   int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static void main(String[] args) {
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
+        // Computation
+        int empcheck = (int) Math.floor(Math.random() * 100) % 2;
+        switch (empcheck){
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage: " + empWage);
+
+    }
+}
